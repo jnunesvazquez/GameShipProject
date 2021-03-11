fa6a3580d5953d013d6c6ef999bebb6850b8d32f
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-03-11 18:56:22 +0100

    Creando clase de estados del juego

---
+++ src/com/project/states/GameState.java
@@ -0,0 +1,16 @@
+package com.project.states;
+
+import java.awt.*;
+
+public class GameState {
+
+    public GameState(){
+
+    }
+    public void update(){
+
+    }
+    public void draw(Graphics g){
+
+    }
+}
