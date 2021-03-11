6cc538ae934f13db7d90df3ea1e8f06e6dd1752a
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-03-11 10:53:48 +0100

    Canvas creado y doble buffering en proceso

--- src/com/project/graphics/Window.java
+++ src/com/project/graphics/Window.java
@@ -1,10 +1,19 @@
 package com.project.graphics;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-public class Window extends JFrame {                            //JFrame nos permitira crear nuestra ventana de juego
+public class Window extends JFrame implements Runnable{         //JFrame nos permitira crear nuestra ventana de juego y Runnable para implementar un subproceso y no sobrecargar el JFrame
 
     public static final int WIDTH = 800, HEIGTH = 500;
+    private boolean running;                                    //Variable que nos permite controlar el estado del juego
+
+    private BufferStrategy bs;
+    private Graphics g;                                         //Clase que nos permite dibujar
+
+    private Canvas canvas;                                      //Instanciamos la clase canvas que nos permite dibujar sobre nuestra ventana o incluso captar eventos de teclado
+    private Thread thread;                                      //Instanciamos la clase de hilos para crear un subproceso en el metodo run()
 
     /**
      * Constructor de nuestra ventana
@@ -16,5 +25,53 @@ public class Window extends JFrame {                            //JFrame nos per
         setResizable(false);                                    //Metodo que permite o no redimensionar la ventana
         setLocationRelativeTo(null);                            //Metodo para definir la visualizacion de la ventana en el centro de la pantalla
         setVisible(true);                                       //Metodo para que la ventana sea visible
+
+        canvas = new Canvas();
+
+        canvas.setPreferredSize(new Dimension(WIDTH, HEIGTH));  //Metodo que define el tamaño de nuestro rectangulo
+        canvas.setMaximumSize(new Dimension(WIDTH, HEIGTH));    //Metodo que define el tamaño maximo de nuestro rectangulo
+        canvas.setMinimumSize(new Dimension(WIDTH, HEIGTH));    //Metodo que define el tamaño minimo de nuestro rectangulo
+        canvas.setFocusable(true);                              //Metodo que permita interactuar con el rectangulo
+
+        add(canvas);
+    }
+
+    public void update(){
+
+    }
+
+    public void draw(){
+        bs = canvas.getBufferStrategy();                        //Le pasamos al canvas el buffer strategy pero nos devuelve un nulo
+        if (bs == null) {
+            canvas.createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+        g.dispose();
+        bs.show();
+    }
+
+    @Override
+    public void run() {
+        while (running){
+            update();
+            draw();
+        }
+        stop();
+    }
+
+    public void start(){
+        thread = new Thread(this);                      //Objeto de la clase Thread que recibe por parametro la implementacion de Runnable
+        thread.start();                                         //Declaramos el comienzo del hilo
+        running = true;
+    }
+
+    public void stop(){
+        try {
+            thread.join();                                      //El estado del hilo pasaria a un estado de espera
+            running = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();                                //Metodo que imprime informacion de un error incluyendo las clases de donde proceden
+        }
     }
 }
--- src/com/project/main/Main.java
+++ src/com/project/main/Main.java
@@ -5,6 +5,6 @@ import com.project.graphics.*;
 public class Main {
 
     public static void main(String[] args) {
-	new Window();
+	new Window().start();
     }
 }
